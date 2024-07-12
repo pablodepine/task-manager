dad44b5e5cbd75355be7b74be2d28d899909bd30
Pablo Depiné <pablodepine96@gmail.com>
2024-07-12 08:55:08 -0300

    feat: cria rota cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent,
   },
 ];
